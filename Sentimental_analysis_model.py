1e9f22928660d79682bd8769fea8a57abe7a5a6f
Kishore-KS <58883147+Kishore-KS@users.noreply.github.com>
2021-06-16 00:21:20 +0530

    reshuffling

--- Sentimental_analysis_model.py
+++ Sentimental_analysis_model.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-import methods
 
 #Reading the training data
 dataset = pd.read_csv("./Train/Train.csv")
@@ -11,7 +10,25 @@ le = LabelEncoder()
 y = dataset['label'].values
 y = le.fit_transform(y)
 
-dataset['cleaned_review'] =  dataset['review'].apply(methods.clean_text)
+import re
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+
+sw = set(stopwords.words('english'))
+ps = PorterStemmer()
+
+def clean_text(sample):
+    sample = sample.lower()
+    #removing all <br> tags in the review
+    sample = sample.replace("<br /><br />", "")
+    #removing all special characters from the review
+    sample = re.sub("[^a-zA-Z]+", " ", sample)    
+    sample = sample.split()
+    sample = [ps.stem(s) for s in sample if s not in sw] # list comprehension
+    sample = " ".join(sample)
+    return sample
+
+dataset['cleaned_review'] =  dataset['review'].apply(clean_text)
 corpus = dataset['cleaned_review'].values
 
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
--- methods.py
+++ methods.py
@@ -4,23 +4,6 @@ Created on Tue Jun 15 23:33:14 2021
 
 @author: User
 """
-import re
-from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
-
-sw = set(stopwords.words('english'))
-ps = PorterStemmer()
-
-def clean_text(sample):
-    sample = sample.lower()
-    #removing all <br> tags in the review
-    sample = sample.replace("<br /><br />", "")
-    #removing all special characters from the review
-    sample = re.sub("[^a-zA-Z]+", " ", sample)    
-    sample = sample.split()
-    sample = [ps.stem(s) for s in sample if s not in sw] # list comprehension
-    sample = " ".join(sample)
-    return sample
 
 #Loading the model
 from keras.models import load_model
