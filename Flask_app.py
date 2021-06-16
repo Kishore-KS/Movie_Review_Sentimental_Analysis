7ed375d6c9d13afa4fec808d0fd39f65d7ccfa31
Kishore-KS <58883147+Kishore-KS@users.noreply.github.com>
2021-06-16 18:09:51 +0530

    Fixed return

--- Flask_app.py
+++ Flask_app.py
@@ -4,20 +4,12 @@ Created on Tue Jun 15 21:07:21 2021
 
 @author: User
 """
-import Predictions
-
-'''string_input = input("Enter your review: ")
-string_df = pd.DataFrame([string_input],columns=["review"])
-y_pred = sam.predictions(string_df)
-sam.review_classification(y_pred)'''
 
 from flask import Flask, request, render_template 
-  
-# Flask constructor
-app = Flask(__name__)   
-  
-# A decorator used to tell the application
-# which URL is associated function
+app = Flask(__name__)
+
+import Predictions
+
 @app.route('/', methods =["GET", "POST"])
 def gfg():
     if request.method == "POST":
@@ -25,7 +17,7 @@ def gfg():
        review = request.form.get("review")
        pred = Predictions.predictions(review)
        review_class = Predictions.review_classification(pred)
-       return "The review has a ",review_class[0]," probability of being ",review_class[1]
+       return "The review has a "+review_class[0]+" probability of being "+review_class[1]
     return render_template("index.html")
   
 if __name__=='__main__':
