508dc1c590488182d5f81178a7331bde83aeaa12
Kishore-KS <58883147+Kishore-KS@users.noreply.github.com>
2021-06-16 17:38:07 +0530

    Changed output

--- Flask_app.py
+++ Flask_app.py
@@ -4,7 +4,7 @@ Created on Tue Jun 15 21:07:21 2021
 
 @author: User
 """
-import methods
+import Predictions
 
 '''string_input = input("Enter your review: ")
 string_df = pd.DataFrame([string_input],columns=["review"])
@@ -23,9 +23,9 @@ def gfg():
     if request.method == "POST":
        # getting input with name = fname in HTML form
        review = request.form.get("review")
-       y_pred = methods.predictions(review)
-       review_class = methods.review_classification(y_pred)
-       return "Your review classification ",y_pred," ",review_class
+       pred = Predictions.predictions(review)
+       review_class = Predictions.review_classification(pred)
+       return "The review has a ",review_class[0]," probability of being ",review_class[1]
     return render_template("index.html")
   
 if __name__=='__main__':
